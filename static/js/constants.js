7df104691eba154a5af89c79b3d46189ebb46b23
jacuna88 <60046469+jacuna88@users.noreply.github.com>
2022-03-01 18:46:55 -0500

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,4 +1,4 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "https://8534-186-154-134-91.ngrok.io/webhooks/rest/webhook";
+const rasa_server_url = "https://4897-186-154-134-91.ngrok.io/webhooks/rest/webhook";
 
 const sender_id = uuidv4();
