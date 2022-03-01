f488e170d792c0600c64c067ea6c8ac0185c47a0
jacuna88 <jeacunag@gmail.com>
2022-03-01 00:55:49 -0500

    initial commit

---
+++ static/js/constants.js
@@ -0,0 +1,5 @@
+const action_name = "action_hello_world";
+const rasa_server_url = "https://n4qgczfrbg.execute-api.us-east-1.amazonaws.com/prod/webhooks/rest/webhook";
+const rasa_root_url = "https://n4qgczfrbg.execute-api.us-east-1.amazonaws.com/prod";
+//const rasa_server_url = "https://9ef4-186-154-134-91.ngrok.io/webhooks/rest/webhook";
+const sender_id = uuidv4();
