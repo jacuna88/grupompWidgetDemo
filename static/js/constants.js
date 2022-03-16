b3a4b549a5ffe7a599d918cc4d175a3f436024b8
Julian Acuna-Gonzalez <60046469+jacuna88@users.noreply.github.com>
2022-03-15 22:57:51 -0500

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,4 +1,4 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "https://1533-201-244-232-27.ngrok.io/webhooks/rest/webhook";
+const rasa_server_url = "https://34.72.127.114:5010/webhooks/rest/webhook";
 
 const sender_id = uuidv4();
